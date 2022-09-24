9de5c3ce4bd97440dd22c66a4376e5c16c3525fe
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-09-24 10:31:34 +0200

    final testing

--- test/config.js
+++ test/config.js
@@ -1,6 +1,6 @@
 module.exports  = {
     username: "Admin",
     password: "admin123",
-    newusername: "Oleksandr_Kryvchenko",
+    newusername: "Oleksandr.Kryvchenko",
     user_pwd: "K$mata12345"
 }

9ec4c2b71d9edf5e1c1d03bdd93863a29ee5d807
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-09-25 10:39:01 +0200

    randomnes added to the user name

--- test/config.js
+++ test/config.js
@@ -1,6 +1,6 @@
 module.exports  = {
     username: "Admin",
     password: "admin123",
-    newusername: "Oleksandr.Kryvchenko",
+    newusername: `Oleksandr.Kryvchenko${Math.floor(Math.random() * 999)}`,
     user_pwd: "K$mata12345"
 }
